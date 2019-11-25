7bdbbaf41555e9f4fc5effbd27cf32ea3e3a0440
Ashwin Phatak <ashwinpphatak@gmail.com>
2019-11-25 15:52:50 +0530

    Support export/import records to/from genesis file.

--- x/nameservice/genesis.go
+++ x/nameservice/genesis.go
@@ -7,9 +7,18 @@ package nameservice
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/wirelineio/wns/x/nameservice/internal/types"
 )
 
-type GenesisState struct{}
+type NameEntry struct {
+	Name  string           `json:"name" yaml:"name"`
+	Entry types.NameRecord `json:"record" yaml:"record"`
+}
+
+type GenesisState struct {
+	Names   []NameEntry       `json:"names" yaml:"names"`
+	Records []types.RecordObj `json:"records" yaml:"records"`
+}
 
 func NewGenesisState() GenesisState {
 	return GenesisState{}
@@ -24,9 +33,29 @@ func DefaultGenesisState() GenesisState {
 }
 
 func InitGenesis(ctx sdk.Context, keeper Keeper, data GenesisState) []abci.ValidatorUpdate {
+	for _, nameEntry := range data.Names {
+		keeper.SetNameRecord(ctx, nameEntry.Name, nameEntry.Entry)
+	}
+
+	for _, record := range data.Records {
+		keeper.PutRecord(ctx, record.ToRecord())
+	}
+
 	return []abci.ValidatorUpdate{}
 }
 
-func ExportGenesis(ctx sdk.Context, k Keeper) GenesisState {
-	return GenesisState{}
+func ExportGenesis(ctx sdk.Context, keeper Keeper) GenesisState {
+	names := keeper.ListNameRecords(ctx)
+	nameEntries := []NameEntry{}
+	for name, nameRecord := range names {
+		nameEntries = append(nameEntries, NameEntry{name, nameRecord})
+	}
+
+	records := keeper.ListRecords(ctx)
+	recordEntries := []types.RecordObj{}
+	for _, record := range records {
+		recordEntries = append(recordEntries, record.ToRecordObj())
+	}
+
+	return GenesisState{nameEntries, recordEntries}
 }
