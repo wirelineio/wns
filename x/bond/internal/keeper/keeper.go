2cc5cf0986ea0a2153fd8133b5291686bc768500
Ashwin Phatak <ashwinpphatak@gmail.com>
2019-12-11 12:18:22 +0530

    Implement bond creation.

--- x/bond/internal/keeper/keeper.go
+++ x/bond/internal/keeper/keeper.go
@@ -7,12 +7,25 @@ package keeper
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/cosmos/cosmos-sdk/x/supply"
+	"github.com/wirelineio/wns/x/bond/internal/types"
 )
+
+// prefixIDToBondIndex is the prefix for ID -> Bond index in the KVStore.
+// Note: This is the primary index in the system.
+// Note: Golang doesn't support const arrays.
+var prefixIDToBondIndex = []byte{0x00}
+
+// prefixOwnerToBondsIndex is the prefix for the Owner -> [Bond] index in the KVStore.
+var prefixOwnerToBondsIndex = []byte{0x01}
 
 // Keeper maintains the link to storage and exposes getter/setter methods for the various parts of the state machine
 type Keeper struct {
-	CoinKeeper bank.Keeper
+	AccountKeeper auth.AccountKeeper
+	CoinKeeper    bank.Keeper
+	SupplyKeeper  supply.Keeper
 
 	storeKey sdk.StoreKey // Unexposed key to access store from sdk.Context
 
@@ -20,10 +33,25 @@ type Keeper struct {
 }
 
 // NewKeeper creates new instances of the bond Keeper
-func NewKeeper(coinKeeper bank.Keeper, storeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
+func NewKeeper(accountKeeper auth.AccountKeeper, coinKeeper bank.Keeper, supplyKeeper supply.Keeper, storeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
 	return Keeper{
-		CoinKeeper: coinKeeper,
-		storeKey:   storeKey,
-		cdc:        cdc,
+		AccountKeeper: accountKeeper,
+		CoinKeeper:    coinKeeper,
+		SupplyKeeper:  supplyKeeper,
+		storeKey:      storeKey,
+		cdc:           cdc,
 	}
+}
+
+// CreateBond - saves a bond to the store.
+func (k Keeper) CreateBond(ctx sdk.Context, bond types.Bond) {
+	store := ctx.KVStore(k.storeKey)
+
+	// Bond ID -> Bond index.
+	store.Set(append(prefixIDToBondIndex, []byte(bond.ID)...), k.cdc.MustMarshalBinaryBare(bond))
+
+	// Owner -> [Bond] index.
+	var key = append(prefixOwnerToBondsIndex, []byte(bond.Owner)...)
+	key = append(key, []byte(bond.ID)...)
+	store.Set(key, []byte{})
 }
