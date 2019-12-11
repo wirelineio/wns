df1b77c8012890e604c61dd77b03bb02210d7970
Ashwin Phatak <ashwinpphatak@gmail.com>
2019-12-11 13:15:16 +0530

    List/Get bond commands.

--- x/bond/client/cli/query.go
+++ x/bond/client/cli/query.go
@@ -5,9 +5,13 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/wirelineio/wns/x/bond/internal/types"
 )
 
@@ -20,6 +24,56 @@ func GetQueryCmd(storeKey string, cdc *codec.Codec) *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
-	bondQueryCmd.AddCommand(client.GetCommands()...)
+	bondQueryCmd.AddCommand(client.GetCommands(
+		GetCmdList(storeKey, cdc),
+		GetCmdGetBond(storeKey, cdc),
+	)...)
 	return bondQueryCmd
+}
+
+// GetCmdList queries all bonds.
+func GetCmdList(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "list",
+		Short: "List bonds.",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			viper.Set("trust-node", true)
+
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/list", queryRoute), nil)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(res))
+
+			return nil
+		},
+	}
+}
+
+// GetCmdGetBond queries a bond.
+func GetCmdGetBond(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "get [ID]",
+		Short: "Get bond.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			viper.Set("trust-node", true)
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			id := args[0]
+
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/get/%s", queryRoute, id), nil)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(res))
+
+			return nil
+		},
+	}
 }
--- x/bond/internal/keeper/querier.go
+++ x/bond/internal/keeper/querier.go
@@ -5,16 +5,60 @@
 package keeper
 
 import (
+	"encoding/json"
+	"strings"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/wirelineio/wns/x/bond/internal/types"
+)
+
+// query endpoints supported by the bond Querier
+const (
+	ListBonds = "list"
+	GetBond   = "get"
 )
 
 // NewQuerier is the module level router for state queries
 func NewQuerier(keeper Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err sdk.Error) {
 		switch path[0] {
+		case ListBonds:
+			return listBonds(ctx, path[1:], req, keeper)
+		case GetBond:
+			return getBond(ctx, path[1:], req, keeper)
 		default:
 			return nil, sdk.ErrUnknownRequest("unknown bond query endpoint")
 		}
 	}
+}
+
+// nolint: unparam
+func listBonds(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) (res []byte, err sdk.Error) {
+	bonds := keeper.ListBonds(ctx)
+
+	bz, err2 := json.MarshalIndent(bonds, "", "  ")
+	if err2 != nil {
+		panic("Could not marshal result to JSON.")
+	}
+
+	return bz, nil
+}
+
+// nolint: unparam
+func getBond(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) (res []byte, err sdk.Error) {
+
+	id := types.ID(strings.Join(path, "/"))
+	if !keeper.HasBond(ctx, id) {
+		return nil, sdk.ErrUnknownRequest("Bond not found.")
+	}
+
+	bond := keeper.GetBond(ctx, id)
+
+	bz, err2 := json.MarshalIndent(bond, "", "  ")
+	if err2 != nil {
+		panic("Could not marshal result to JSON.")
+	}
+
+	return bz, nil
 }
