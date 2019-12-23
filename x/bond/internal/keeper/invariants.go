c9d5f60f3226e3941d5b5100c7e1d122009a62ff
Ashwin Phatak <ashwinpphatak@gmail.com>
2019-12-23 17:59:34 +0530

    Bond and nameservice module invariants.

--- x/bond/internal/keeper/invariants.go
+++ x/bond/internal/keeper/invariants.go
@@ -5,10 +5,39 @@
 package keeper
 
 import (
+	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/wirelineio/wns/x/bond/internal/types"
 )
 
 // RegisterInvariants registers all bond module invariants.
 func RegisterInvariants(ir sdk.InvariantRegistry, k Keeper) {
+	ir.RegisterRoute(types.ModuleName, "module-accounts", BalanceInvariants(k))
+}
 
+// BalanceInvariants checks that the 'bond' and 'rent' module account balances are non-negative.
+func BalanceInvariants(k Keeper) sdk.Invariant {
+	return func(ctx sdk.Context) (string, bool) {
+		res, stop := checkModuleNegativeBalance(ctx, k, types.ModuleName)
+		if stop {
+			return res, stop
+		}
+
+		return checkModuleNegativeBalance(ctx, k, types.RecordRentModuleAccountName)
+	}
+}
+
+func checkModuleNegativeBalance(ctx sdk.Context, k Keeper, name string) (string, bool) {
+	moduleAccount := k.SupplyKeeper.GetModuleAccount(ctx, types.ModuleName)
+	if moduleAccount.GetCoins().IsAnyNegative() {
+		return sdk.FormatInvariant(types.ModuleName, "module-accounts", fmt.Sprintf("Module account '%s' has negative balance.", name)), true
+	}
+
+	return "", false
+}
+
+func AllInvariants(k Keeper) sdk.Invariant {
+	return func(ctx sdk.Context) (string, bool) {
+		return BalanceInvariants(k)(ctx)
+	}
 }
