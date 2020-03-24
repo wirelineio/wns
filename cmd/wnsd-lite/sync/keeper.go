f02022f4bcaede73109ee824dd884d6fe231b4ea
Ashwin Phatak <ashwinpphatak@gmail.com>
2020-03-24 14:49:56 +0530

    Keeper visibility changes.

--- cmd/wnsd-lite/sync/keeper.go
+++ cmd/wnsd-lite/sync/keeper.go
@@ -13,13 +13,13 @@ import (
 
 // Keeper is an impl. of an interface similar to the nameservice Keeper.
 type Keeper struct {
-	Codec *amino.Codec
-	Store store.KVStore
+	codec *amino.Codec
+	store store.KVStore
 }
 
 // NewKeeper creates a new keeper.
 func NewKeeper(codec *amino.Codec, store store.KVStore) *Keeper {
-	return &Keeper{Codec: codec, Store: store}
+	return &Keeper{codec: codec, store: store}
 }
 
 // Status represents the sync status of the node.
@@ -29,51 +29,51 @@ type Status struct {
 
 // HasStatusRecord checks if the store has a status record.
 func (k Keeper) HasStatusRecord() bool {
-	return k.Store.Has(ns.KeySyncStatus)
+	return k.store.Has(ns.KeySyncStatus)
 }
 
 // GetStatusRecord gets the sync status record.
 func (k Keeper) GetStatusRecord() Status {
-	bz := k.Store.Get(ns.KeySyncStatus)
+	bz := k.store.Get(ns.KeySyncStatus)
 	var status Status
-	k.Codec.MustUnmarshalBinaryBare(bz, &status)
+	k.codec.MustUnmarshalBinaryBare(bz, &status)
 
 	return status
 }
 
 // SaveStatus saves the sync status record.
 func (k Keeper) SaveStatus(status Status) {
-	bz := k.Codec.MustMarshalBinaryBare(status)
-	k.Store.Set(ns.KeySyncStatus, bz)
+	bz := k.codec.MustMarshalBinaryBare(status)
+	k.store.Set(ns.KeySyncStatus, bz)
 }
 
 // HasRecord - checks if a record by the given ID exists.
 func (k Keeper) HasRecord(id ns.ID) bool {
-	return ns.HasRecord(k.Store, id)
+	return ns.HasRecord(k.store, id)
 }
 
 // GetRecord - gets a record from the store.
 func (k Keeper) GetRecord(id ns.ID) ns.Record {
-	return ns.GetRecord(k.Store, k.Codec, id)
+	return ns.GetRecord(k.store, k.codec, id)
 }
 
 // PutRecord - saves a record to the store and updates ID -> Record index.
 func (k Keeper) PutRecord(record nameservice.RecordObj) {
-	k.Store.Set(nameservice.GetRecordIndexKey(record.ID), k.Codec.MustMarshalBinaryBare(record))
+	k.store.Set(nameservice.GetRecordIndexKey(record.ID), k.codec.MustMarshalBinaryBare(record))
 }
 
 // SetNameRecord - sets a name record.
 func (k Keeper) SetNameRecord(wrn string, nameRecord nameservice.NameRecord) {
-	k.Store.Set(nameservice.GetNameRecordIndexKey(wrn), k.Codec.MustMarshalBinaryBare(nameRecord))
+	k.store.Set(nameservice.GetNameRecordIndexKey(wrn), k.codec.MustMarshalBinaryBare(nameRecord))
 }
 
 // ResolveWRN resolves a WRN to a record.
 // Note: Version part of the WRN might have a semver range.
 func (k Keeper) ResolveWRN(wrn string) *ns.Record {
-	return ns.ResolveWRN(k.Store, k.Codec, wrn)
+	return ns.ResolveWRN(k.store, k.codec, wrn)
 }
 
 // MatchRecords - get all matching records.
 func (k Keeper) MatchRecords(matchFn func(*ns.Record) bool) []*ns.Record {
-	return ns.MatchRecords(k.Store, k.Codec, matchFn)
+	return ns.MatchRecords(k.store, k.codec, matchFn)
 }
