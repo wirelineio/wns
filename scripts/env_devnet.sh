e4fc385884ec29945170e69b5216cd3b11a3f655
Ashwin Phatak <ashwin@wireline.io>
2020-04-06 15:23:45 +0530

    Devnet WNS script. (#227)

---
+++ scripts/env_devnet.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# TODO(ashwin): To switch to https, we have to distribute self-signed cert to clients.
+export WIRE_WNS_ENDPOINT="http://wns1.bozemanpass.net:9473/graphql"
+
+if [[ -z "$WIRE_WNS_USER_KEY" ]]; then
+  echo "WNS user key not found. Set WIRE_WNS_USER_KEY and try again."
+  return
+fi
+
+export WIRE_WNS_BOND_ID=
+NUM_BONDS=$(wire wns bond list | jq -e ". | length")
+if [ "$NUM_BONDS" -eq "0" ]; then
+  wire wns bond create --type uwire --quantity 10000000000 > /dev/null
+fi
+
+export WIRE_WNS_BOND_ID=$(wire wns bond list | jq -r ".[0].id")
+
+echo WIRE_WNS_ENDPOINT=${WIRE_WNS_ENDPOINT}
+echo WIRE_WNS_USER_KEY=${WIRE_WNS_USER_KEY}
+echo WIRE_WNS_BOND_ID=${WIRE_WNS_BOND_ID}
