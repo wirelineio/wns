bf362eb807af0ee48b5f43943a61a98ef6fcb1ee
jakub_jozwiak <kubencki@gmail.com>
2020-07-30 17:19:35 +0200

    Add basic docs setup

---
+++ docs/content/templates.md
@@ -0,0 +1,6 @@
+---
+title: Templates
+description: Extending DXOS documentation
+---
+
+Copy this template to create a new docs site.
---
+++ docs/gatsby-config.js
@@ -0,0 +1,41 @@
+//
+// Copyright 2020 DXOS.org
+//
+
+const themeOptions = require('@dxos/docs-theme/theme-options');
+
+module.exports = {
+  pathPrefix: '/wns',
+  plugins: [
+    {
+      resolve: 'gatsby-theme-apollo-docs',
+      options: {
+        ...themeOptions,
+        root: __dirname,
+        githubRepo: 'wirelineio/wns',
+        description: 'DXOS - The Decentralized Operating System',
+        subtitle: 'DXOS WNS',
+        sidebarCategories: {
+          null: [
+            'index',
+            'templates'
+          ]
+        }
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/assets/img`
+      }
+    },
+
+    // Image processing
+    // https://www.gatsbyjs.org/packages/gatsby-plugin-sharp
+    // https://www.gatsbyjs.org/packages/gatsby-transformer-sharp
+    // https://github.com/lovell/sharp
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp'
+  ]
+};
