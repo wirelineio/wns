cbe827a1c42153bdb7b7208ca678eba98bb8caf2
Ashwin Phatak <ashwinpphatak@gmail.com>
2020-08-13 18:14:23 +0530

    Auction module types.

--- x/auction/internal/types/types.go
+++ x/auction/internal/types/types.go
@@ -8,18 +8,75 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// Auction status values.
+const (
+	// Auction is in commit phase.
+	AuctionStatusCommitPhase = 0x1
+
+	// Auction is in reveal phase.
+	AuctionStatusRevealPhase = 0x2
+
+	// Auction finished (i.e. winner selected).
+	AuctionStatusFinished = 0xf
+)
+
+// Bid status values.
+const (
+	BidStatusCommitted = 0x1
+	BidStatusRevealed  = 0x2
+	BidStatusExpired   = 0x3
 )
 
 // ID for auctions.
 type ID string
 
-// Auction represents funds deposited by an account for record rent payments.
+// Auction is a 2nd price sealed-bid on-chain auction.
 type Auction struct {
-	ID      ID        `json:"id,omitempty"`
-	Owner   string    `json:"owner,omitempty"`
-	Balance sdk.Coins `json:"balance"`
+	ID     ID   `json:"id,omitempty"`
+	Status int8 `json:"status,omitempty"`
+
+	// Auction create time.
+	CreateTime time.Time `json:"createTime,omitempty"`
+
+	// Time when the commit phase ends.
+	CommitsEndTime time.Time `json:"commitsEndTime,omitempty"`
+
+	// Time when the reveal phase ends.
+	RevealsEndTime time.Time `json:"revealsEndTime,omitempty"`
+
+	// Commit Fee + Reveal Fee both need to be paid when committing a bid.
+	// Reveal Fee is returned ONLY if the bid is revealed.
+	CommitFee sdk.Coin `json:"commitFee,omitempty"`
+	RevealFee sdk.Coin `json:"revealFee,omitempty"`
+
+	// Minimum bid for a valid commit.
+	MinimumBid sdk.Coin `json:"minimumBid,omitempty"`
+
+	// Winner address.
+	WinnerAddress sdk.Address `json:"winnerAddress,omitempty"`
+
+	// Winning bid, i.e. highest bid.
+	WinnerBid sdk.Coin `json:"winnerBid,omitempty"`
+
+	// Amount winner actually pays, i.e. 2nd highest bid.
+	// As it's a 2nd price auction.
+	WinnerPrice sdk.Address `json:"winnerPrice,omitempty"`
+}
+
+// Bid represents a sealed bid (commit) made during the auction.
+type Bid struct {
+	AuctionID     ID          `json:"auctionId,omitempty"`
+	BidderAddress sdk.Address `json:"bidderAddress,omitempty"`
+	Status        int8        `json:"status,omitempty"`
+	BidAmount     sdk.Coin    `json:"bidAmount,omitempty"`
+	AuctionFee    sdk.Coin    `json:"auctionFee,omitempty"`
+	CommitTime    time.Time   `json:"commitTime,omitempty"`
+	RevealTime    time.Time   `json:"revealTime,omitempty"`
 }
 
 // AuctionID simplifies generation of auction IDs.
